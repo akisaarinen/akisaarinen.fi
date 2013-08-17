40d519b4ec937f6587fbb1d2b73314d4e0a0938e
Aki Saarinen <aki@akisaarinen.fi>
2013-08-17 23:54:49 +0300

    Enable deploy with script
    Signed-off-by: Aki Saarinen <aki@akisaarinen.fi>

--- deploy.sh
+++ deploy.sh
@@ -18,13 +18,13 @@ $DIR/jekyll.sh build
 popd
 
 echo "Uploading assets..."
-#ssh kapsi.fi "rm -rf $WEB/assets.new"
-#scp -r $SITE/assets kapsi.fi:$WEB/assets.new
+ssh kapsi.fi "rm -rf $WEB/assets.new"
+scp -r $SITE/assets kapsi.fi:$WEB/assets.new
 
 echo "Taking assets into use..."
-#ssh kapsi.fi "mv $WEB/assets $WEB/assets-$DATE && mv $WEB/assets.new $WEB/assets"
+ssh kapsi.fi "mv $WEB/assets $WEB/assets-$DATE && mv $WEB/assets.new $WEB/assets"
 
 echo "Uploading new markup"
-#scp -r $SITE/*.html $SITE/*.xml $SITE/blog/ $SITE/projects/ $SITE/blog-archive/ kapsi.fi:$WEB/
+scp -r $SITE/*.html $SITE/*.xml $SITE/blog/ $SITE/projects/ $SITE/blog-archive/ kapsi.fi:$WEB/
 
 echo "Deploy finished"
